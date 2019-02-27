9ab762db99c96976ffbb6005af06b9c00374700f
Carlos Li Hu <carloslihu96@gmail.com>
2019-02-27 20:21:48 +0100

    Probando mapa leaflet
    creado requirements.txt

--- codigo/README.rst
+++ codigo/README.rst
@@ -1,2 +1,12 @@
 # TFG
 Detección de patrones y trayectorias a partir de información temporal y geoespacial poco precisa
+
+instalar postgresql (lo escojo?)
+	(geolocation permite, a partir de ip, obtener pubs más cercanos
+	integrar postgres en python)
+
+    intentar crear BBDD postgres
+
+mirar leaflet offline
+
+requirements.txt
--- codigo/flaskr/__init__.py
+++ codigo/flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -26,9 +26,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    #@app.route('/hello')
+    #def hello():
+    #    return render_template('hello.html')
 
     # register the database commands
     from flaskr import db
